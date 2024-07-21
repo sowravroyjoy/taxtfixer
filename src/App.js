ef9c44c097ae30c2d85e6b904ece020d94cf11ba
Sowrav Roy Joy <83823385+sowravroyjoy@users.noreply.github.com>
2024-07-21 10:45:14 -0400

    removing react router to optimize for github pages

--- src/App.js
+++ src/App.js
@@ -1,45 +1,62 @@
-import react, { useState } from 'react';
-import './App.css';
-import About from './components/About';
-import Navbar from './components/Navbar';
-import TextForm from './components/TextForm';
-import Alert from './components/Alert';
+import { useState } from "react";
+import "./App.css";
+// import About from "./components/About";
+import Navbar from "./components/Navbar";
+import TextForm from "./components/TextForm";
+import Alert from "./components/Alert";
+import * as React from "react";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  const [mode, setMode] = useState('light');
+  const [mode, setMode] = useState("light");
   const [alert, setAlert] = useState(null);
 
-  const showAlert = (message, type) =>{
+  const showAlert = (message, type) => {
     setAlert({
       msg: message,
-      type: type
-    })
-    setTimeout(()=>{
+      type: type,
+    });
+    setTimeout(() => {
       setAlert(null);
     }, 1000);
-  }
+  };
 
-  const toggleMode = () =>{
-    if(mode === 'light'){
-      setMode('dark')
-      document.body.style.color = 'white'
-      document.body.style.backgroundColor = '#212529'
-      showAlert("Dark mode has been enabled", "success")
-    }else{
-      setMode('light')
-      document.body.style.color = 'black'
-      document.body.style.backgroundColor = 'white'
-      showAlert("Light mode has been enabled", "success")
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.color = "white";
+      document.body.style.backgroundColor = "#212529";
+      showAlert("Dark mode has been enabled", "success");
+    } else {
+      setMode("light");
+      document.body.style.color = "black";
+      document.body.style.backgroundColor = "white";
+      showAlert("Light mode has been enabled", "success");
     }
-  }
+  };
   return (
     <>
-      <Navbar title = "TextFixer" mode={mode} toggleMode={toggleMode}/>
-      <Alert alert={alert}/>
+    {/* <Router> */}
+      <Navbar title="TextFixer" mode={mode} toggleMode={toggleMode} />
+      <Alert alert={alert} />
       <div className="container my-3">
-      <TextForm heading = "Enter your text to analyze" mode={mode} showAlert={showAlert}/>
-      {/* <About/> */}
-        </div>
+        
+          {/* <Routes>
+            <Route
+              exact path="/"
+              element={ */}
+                <TextForm
+                  heading="Enter your text to analyze"
+                  mode={mode}
+                  showAlert={showAlert}
+                />
+              {/* }
+            />
+            <Route exact path="/about" element={<About />} />
+          </Routes> */}
+        
+      </div>
+      {/* </Router> */}
     </>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,15 @@
 import React from "react";
 import PropTypes from "prop-types";
+// import { Link } from "react-router-dom";
 
 export default function Navbar(props) {
   return (
+    <>
     <nav
       className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}
     >
       <div className="container-fluid">
-        <a className="navbar-brand" href="/">
+        <a className="navbar-brand" href="#">
           {props.title}
         </a>
         <button
@@ -24,24 +26,16 @@ export default function Navbar(props) {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link active" aria-current="page" href="/">
+              <a className="nav-link active" aria-current="page" href="#">
                 Home
               </a>
             </li>
-            <li className="nav-item">
-              <a className="nav-link" href="/">
+            {/* <li className="nav-item">
+              <a className="nav-link" href="/about">
                 About Us
               </a>
-            </li>
+            </li> */}
           </ul>
-          {/* <form className="d-flex" role="search">
-            <input
-              className="form-control me-2"
-              type="search"
-              placeholder="Search"
-              aria-label="Search"
-            />
-          </form> */}
           <div className={`form-check form-switch text-${props.mode==='light'?'dark':'light'}`}>
               <input
                 className="form-check-input"
@@ -57,6 +51,7 @@ export default function Navbar(props) {
         </div>
       </div>
     </nav>
+    </>
   );
 }
 
