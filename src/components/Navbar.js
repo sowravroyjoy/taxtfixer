7f684f001d7e68c141aec14c70e66c6cd12860a4
Sowrav Roy Joy <83823385+sowravroyjoy@users.noreply.github.com>
2024-07-21 12:04:42 -0400

    fixing some bugs

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,44 +5,52 @@ import { Link } from "react-router-dom";
 export default function Navbar(props) {
   return (
     <>
-    <nav
-      className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}
-    >
-      <div className="container-fluid">
-        <Link className="navbar-brand" to="/textfixer">
-          {props.title}
-        </Link>
-        {/* <a className="navbar-brand" to="#">
+      <nav
+        className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}
+      >
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/textfixer">
+            {props.title}
+          </Link>
+          {/* <a className="navbar-brand" to="#">
           {props.title}
         </a> */}
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-            <li className="nav-item">
-              <Link className="nav-link active" aria-current="page" to="/">
-                Home
-              </Link>
-              {/* <a className="nav-link active" aria-current="page" href="#">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item">
+                <Link
+                  className="nav-link active"
+                  aria-current="page"
+                  to="/textfixer"
+                >
+                  Home
+                </Link>
+                {/* <a className="nav-link active" aria-current="page" href="#">
                 Home
               </a> */}
-            </li>
-            <li className="nav-item">
-              <Link className="nav-link" to="/about">
-                About Us
-              </Link>
-            </li>
-          </ul>
-          <div className={`form-check form-switch text-${props.mode==='light'?'dark':'light'}`}>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/about">
+                  About Us
+                </Link>
+              </li>
+            </ul>
+            <div
+              className={`form-check form-switch text-${
+                props.mode === "light" ? "dark" : "light"
+              }`}
+            >
               <input
                 className="form-check-input"
                 onClick={props.toggleMode}
@@ -50,13 +58,16 @@ export default function Navbar(props) {
                 role="switch"
                 id="flexSwitchCheckDefault"
               />
-              <label className="form-check-label" htmlFor="flexSwitchCheckDefault">
+              <label
+                className="form-check-label"
+                htmlFor="flexSwitchCheckDefault"
+              >
                 Enable Dark Mode
               </label>
             </div>
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
     </>
   );
 }
