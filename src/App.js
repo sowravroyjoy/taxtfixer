a644a086c400d83f6395683ac42a79211895850c
Sowrav Roy Joy <83823385+sowravroyjoy@users.noreply.github.com>
2024-07-21 12:10:53 -0400

    fixing some bugs

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ function App() {
   };
   return (
     <>
-      <Router basename="/textfixer">
+      <Router>
         <Navbar title="TextFixer" mode={mode} toggleMode={toggleMode} />
         <Alert alert={alert} />
         <div className="container my-3">
