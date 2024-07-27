0ba7fb9b127052c9c412618ef90fcb7a6a2f177b
Sowrav Roy Joy <83823385+sowravroyjoy@users.noreply.github.com>
2024-07-27 16:34:36 -0400

    added some theme functionality

--- src/App.js
+++ src/App.js
@@ -21,7 +21,19 @@ function App() {
     }, 1000);
   };
 
-  const toggleMode = () => {
+  // const removeBodyClasses= () => {
+  //     document.body.classList.remove('bg-light')
+  //     document.body.classList.remove('bg-dark')
+  //     document.body.classList.remove('bg-primary')
+  //     document.body.classList.remove('bg-danger')
+  //     document.body.classList.remove('bg-success')
+  //     document.body.classList.remove('bg-warning')
+  // }
+
+  const toggleMode = (cls) => {
+    // console.log(cls)
+    // removeBodyClasses()
+    // document.body.classList.add('bg-' + cls)
     if (mode === "light") {
       setMode("dark");
       document.body.style.color = "white";
@@ -33,6 +45,7 @@ function App() {
       document.body.style.backgroundColor = "white";
       showAlert("Light mode has been enabled", "success");
     }
+    // showAlert(cls + " mode has been enabled", "success");
   };
   return (
     <>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -46,6 +46,12 @@ export default function Navbar(props) {
                 </Link>
               </li>
             </ul>
+            {/* <div className="d-flex">
+              <div className="bg-primary rounded mx-2" onClick={()=>{props.toggleMode('primary')}} style={{height: '30px', width: '30px', cursor: 'pointer'}}></div>
+              <div className="bg-danger rounded mx-2" onClick={()=>{props.toggleMode('danger')}} style={{height: '30px', width: '30px', cursor: 'pointer'}}></div>
+              <div className="bg-success rounded mx-2" onClick={()=>{props.toggleMode('success')}} style={{height: '30px', width: '30px', cursor: 'pointer'}}></div>
+              <div className="bg-warning rounded mx-2" onClick={()=>{props.toggleMode('warning')}} style={{height: '30px', width: '30px', cursor: 'pointer'}}></div>
+            </div>   */}
             <div
               className={`form-check form-switch text-${
                 props.mode === "light" ? "dark" : "light"
@@ -53,7 +59,7 @@ export default function Navbar(props) {
             >
               <input
                 className="form-check-input"
-                onClick={props.toggleMode}
+                onClick={()=>{props.toggleMode(null)}}
                 type="checkbox"
                 role="switch"
                 id="flexSwitchCheckDefault"
